819f09b6d00e8fd9c454b94f1ffc68dedb86fce6
Porter Rivers <porterrivers444@gmail.com>
2021-06-22 10:42:55 -0400

    Update print width

--- webpack.dev.config.ts
+++ webpack.dev.config.ts
@@ -3,10 +3,7 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 import ESLintPlugin from 'eslint-webpack-plugin';
 
-import {
-	Configuration as WebpackConfiguration,
-	HotModuleReplacementPlugin,
-} from 'webpack';
+import { Configuration as WebpackConfiguration, HotModuleReplacementPlugin } from 'webpack';
 import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
 
 interface Configuration extends WebpackConfiguration {
@@ -27,11 +24,7 @@ const config: Configuration = {
 				use: {
 					loader: 'babel-loader',
 					options: {
-						presets: [
-							'@babel/preset-env',
-							'@babel/preset-react',
-							'@babel/preset-typescript',
-						],
+						presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
 					},
 				},
 			},
