180767da0c51450e35513d08b346bd62ab61c03f
Porter Rivers <porterrivers444@gmail.com>
2021-07-04 12:50:34 -0400

    Installed Material-UI

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
+import { Container, Typography } from '@material-ui/core';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => <h1>My React and TypeScript App!!!! {new Date().toLocaleDateString()}</h1>;
+const App = () => (
+	<Container>
+		<Typography>My React and Typescript Template.</Typography>
+	</Container>
+);
 
 ReactDOM.render(
 	<React.StrictMode>
