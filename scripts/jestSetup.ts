31d207dd3bbc6134e85f486c00f22db5eaf3575a
Porter Rivers <porterrivers444@gmail.com>
2021-08-28 14:22:54 -0400

    Clear cookies after each test

--- scripts/jestSetup.ts
+++ scripts/jestSetup.ts
@@ -1,8 +1,22 @@
 import { toHaveNoViolations } from 'jest-axe';
+import { cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 expect.extend(toHaveNoViolations);
 
 global.beforeEach(() => {
 	expect.hasAssertions();
+});
+
+const deleteAllCookies = () => {
+	document.cookie.split(';').forEach((cookie) => {
+		const eqPos = cookie.indexOf('=');
+		const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+		document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT';
+	});
+};
+
+global.afterEach(() => {
+	cleanup();
+	deleteAllCookies();
 });
