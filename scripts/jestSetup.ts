4a10ca3a7def23abbf3c839514357bb3d516c253
Porter Rivers <porterrivers444@gmail.com>
2022-09-10 16:46:13 -0400

    Fix deprecation, update ecma version

--- scripts/jestSetup.ts
+++ scripts/jestSetup.ts
@@ -1,6 +1,6 @@
-import { toHaveNoViolations } from 'jest-axe';
-import { cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { cleanup } from '@testing-library/react';
+import { toHaveNoViolations } from 'jest-axe';
 
 expect.extend(toHaveNoViolations);
 
@@ -11,7 +11,7 @@ global.beforeEach(() => {
 const deleteAllCookies = () => {
 	document.cookie.split(';').forEach((cookie) => {
 		const eqPos = cookie.indexOf('=');
-		const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+		const name = eqPos > -1 ? cookie.substring(0, eqPos) : cookie;
 		document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT';
 	});
 };
